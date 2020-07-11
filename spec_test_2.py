dae15a2beb42e92bed42673d405a780c15b5b208
Ed <edwinlu8678@gmail.com>
2020-07-11 15:24:01 -0400

    edited some tests

--- spec_test_2.py
+++ spec_test_2.py
@@ -2,15 +2,16 @@ import seabreeze
 import seabreeze.spectrometers
 from seabreeze.spectrometers import Spectrometer
 
-"""
-Series of try except statements testing each function
-"""
-def try_commands():
+# Step 1 and step 4
+def connect_to_spectrometer():
 	# Test list_devices
 	try:
 		spec_list = seabreeze.spectrometers.list_devices()
-		spec1 = spec_list[0]
-		print(f"The devices listed are: {spec_list}. The spectrometer selected is: {spec1}")
+		if spec_list == []:
+			print("ERROR: No spectrometers listed.")
+		else:
+			spec1 = spec_list[0]
+			print(f"The devices listed are: {spec_list}. The spectrometer selected is: {spec1}")
 	except:
 		print("Error with list_devices function")
 
@@ -38,5 +39,45 @@ def try_commands():
 	except:
 		print("Failed to get wavelengths")
 	print("\n")
-# test for shortest integration time
-# test for trigger modes
+
+# Step 2 and 3
+def check_spectrometer():
+	# Test list_devices
+	try:
+		spec_list = seabreeze.spectrometers.list_devices()
+		if spec_list == []:
+			print("No spectrometers listed.")
+		else:
+			print(f"The devices listed are: {spec_list}.")
+	except:
+		print("Error with list_devices function")
+
+	# Test integrating when it's disconnected but the spectrometers are still listed
+	try:
+		spec1.integration_time_micros(1000)		# insert shortest integration time here
+		wavelengths = spec1.wavelengths()
+		print(f"Wavelengths: {wavelengths}")
+	except:
+		print("Failed to get wavelengths as expected")
+	print("\n")
+"""
+Connect:
+check devices
+connect to spec
+check if both specs are the same thing
+run a command
+
+Disconnected:
+check devices
+run a command
+
+Reconnect:
+check devices
+run a command
+
+Reconnect retry:
+check devices
+connect to spec
+check if both specs are the same thing
+run a command
+"""
--- spec_test_3.py
+++ spec_test_3.py
@@ -2,16 +2,46 @@ import seabreeze
 import seabreeze.spectrometers
 
 """
+Connects the Spectrometer
+"""
+def connect():
+	try:
+		spec_list = seabreeze.spectrometers.list_devices()
+		spec1 = spec_list[0]
+		print("Spectrometer successfully connected")
+	except:
+		print("Spectrometer failed to connect. Try again")
+
+"""
 Tests disconnection during integration
 """
-spec_list = seabreeze.spectrometers.list_devices()
-spec1 = spec_list[0]
-
 def integrate():
 	try:
 		print("\nStarting integration...\n")
-		spec1.integration_time_micros(5000)		# insert shortest integration time here
+		spec1.integration_time_micros(5000000)		# 5 seconds
 		wavelengths = spec1.wavelengths()
 		print(f"The wavelength is: {wavelengths}")
 	except:
 		print("Error during integration")
+
+"""
+Tests to see integration returns the same sized array
+"""
+def compare_size():
+	size_array = set()
+	for _ in range(3):
+		try:
+			spec1.integration_time_micros(5000)		# 5 milliseconds
+			wavelengths = spec1.wavelengths()
+			size_array.add(len(wavelengths))
+			# print(f"The wavelength size is: {len(wavelengths)}")
+		except:
+			print("Error during integration")
+	try:
+		if size_array == 1:
+			print(f"Test Successful... Size of arrays are the same. The size is {size_array[0]}")
+		else:
+			print("Test Failed... Size of arrays are not the same")
+			print(f"Sizes of arrays are: {size_array}")
+	except:
+		print("The spectrometer failed to integrate.")
