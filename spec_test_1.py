2416bab063b9343528cd2d1780335af29435eb80
nschaffin <53974684+nschaffin@users.noreply.github.com>
2020-07-12 16:01:37 -0400

    Test 1 rework and Test 4 bug fixes

--- spec_test_1.py
+++ spec_test_1.py
@@ -1,7 +1,7 @@
 """
 Manages communication with the spectrometer through the SeaBreeze library.
 """
-import time
+import time as tyme
 
 import seabreeze
 import seabreeze.spectrometers
@@ -28,6 +28,7 @@ class Spectrometer():
 		if self.spec == None:
 			self.states_spectrometer = 2
 			for _ in range(3):												# Giving the spectrometer 3 attempts to connect
+				tyme.sleep(1.5)
 				import seabreeze
 				import seabreeze.spectrometers
 				try:
@@ -37,13 +38,62 @@ class Spectrometer():
 						break
 				except:
 					continue
+		tyme.sleep(1)
 		if self.states_spectrometer != 2:
 			return "\nSpectrometer {} successfully connected!\n".format(self.spec)
 		print("\nWARNING: No spectrometer connected, check connection...\n")
 		return None
 
+	def spec_reestablish_connection_without_import(self):
+		if self.spec == None:
+			self.states_spectrometer = 2
+			for _ in range(3):												# Giving the spectrometer 3 attempts to connect
+				tyme.sleep(1.5)
+				try:
+					self.spec = self._setupSpectrometer()
+					print("--------------------------------------")
+					if self.states_spectrometer != 2:
+						break
+				except:
+					continue
+		tyme.sleep(1)
+		if self.states_spectrometer != 2:
+			return "\nSpectrometer {} successfully connected!\n".format(self.spec)
+		print("\nWARNING: No spectrometer connected, check connection...\n")
+		return None
 
 	def __init__(self):
 		# 0 = standby, 1 = integrating, 2 = disconnected
 		self.states_spectrometer = 2
 		self.spec = self._setupSpectrometer()
+
+subtest = input("Please enter which subtest you would like to perform (1, 2, or 3): ")
+
+if subtest == '1':
+	spectrometer = Spectrometer()
+	print("Spectrometers listed: {}\n".format(spectrometer.spec))
+	print("Spectrometer's current state: {}".format(spectrometer.states_spectrometer))
+	spectrometer.spec_reestablish_connection()
+	print("Spectrometer's current state: {}".format(spectrometer.states_spectrometer))
+elif subtest == '2':
+	spectrometer = Spectrometer()
+	print("Spectrometers listed: {}\n".format(spectrometer.spec))
+	print("Spectrometer's current state: {}\n".format(spectrometer.states_spectrometer))
+	input("Please unplug spectrometer before proceeding.\nOnce unplugged, please enter in any character to confirm: ")
+	spectrometer.spec_reestablish_connection()
+	print("Spectrometer's current state: {}\n".format(spectrometer.states_spectrometer))
+	input("Now, please plug spectrometer back in before proceeding.\nOnce plugged in, please enter in any character to confirm: ")
+	spectrometer.spec_reestablish_connection()
+	print("Spectrometer's current state: {}".format(spectrometer.states_spectrometer))
+elif subtest == '3':
+	spectrometer = Spectrometer()
+	print("Spectrometers listed: {}\n".format(spectrometer.spec))
+	print("Spectrometer's current state: {}\n".format(spectrometer.states_spectrometer))
+	input("Please unplug spectrometer before proceeding.\nOnce unplugged, please enter in any character to confirm: ")
+	spectrometer.spec_reestablish_connection_without_import()
+	print("Spectrometer's current state: {}\n".format(spectrometer.states_spectrometer))
+	input("Now, please plug spectrometer back in before proceeding.\nOnce plugged in, please enter in any character to confirm: ")
+	spectrometer.spec_reestablish_connection_without_import()
+	print("Spectrometer's current state: {}".format(spectrometer.states_spectrometer))
+else:
+	print("\nInvalid subtest, please run this program again")
--- spec_test_4.py
+++ spec_test_4.py
@@ -43,7 +43,7 @@ class Spectrometer():
 		return None
 
 
-	def sample(self, milliseconds, trigger):
+	def sample(self, microseconds, trigger):
 		"""
 		This function is used to signal the spectrometer to integrate for a set amount of time.
 
@@ -55,9 +55,7 @@ class Spectrometer():
 		
 		try:
 			self.spec.trigger_mode = trigger 							# Setting the trigger mode to normal
-			self.spec.integration_time_micros(milliseconds) 			# Set integration time for spectrometer
-			self.spectrometer_state.integrate()
-			self.oasis_serial.sendBytes(b'\x01') 						# Sending nominal responce
+			self.spec.integration_time_micros(microseconds) 			# Set integration time for spectrometer
 			try:
 				wavelengths, intensities = self.spec.spectrum() 		# Returns wavelengths and intensities as a 2D array, and begins sampling
 			except:
@@ -65,13 +63,10 @@ class Spectrometer():
 				return 'An error occurred while attempting to sample' 	# Command to sample didn't work properly
 
 			data = wavelengths, intensities 							# Saving 2D array to variable data
+			self.data = data
 			if data == []:
 				return 'No data entered' 								# Error handling for no data collected
-			self.oasis_serial.sendBytes(b'\x30') 						# Code sent to spectrometer signaling sampling has successfully finished
 
-			timestamp = time.time() 									# Returns # of seconds since Jan 1, 1970 (since epoch)
-			self.fm.save_sample(timestamp, data) 						# Function call to create spectrometer file
-			self.spectrometer_state.on_standby()
 		except:
 			self.spec_reestablish_connection()
 			return 'Attempting to Reconnect Spectrometer'
@@ -83,6 +78,7 @@ class Spectrometer():
 		# 0 = standby, 1 = integrating, 2 = disconnected
 		self.states_spectrometer = 2
 		self.spec = self._setupSpectrometer()
+		self.data = []
 
 
 spectrometer = Spectrometer()
@@ -94,12 +90,15 @@ while True:
 	if t_mode == '0':
 		spectrometer.sample(time, t_mode)		# Normal Trigger
 		print("Spectrometer's Current Status: {}\n".format(spectrometer.states_spectrometer))
+		print("Spectrometer's collected data: {}".format(spectrometer.data))
 	elif t_mode == '1':
 		spectrometer.sample(time, t_mode)		# External Hardware Level Trigger Mode
 		print("Spectrometer's Current Status: {}\n".format(spectrometer.states_spectrometer))
+		print("Spectrometer's collected data: {}".format(spectrometer.data))
 	elif t_mode == '3': 
 		spectrometer.sample(time, t_mode)		# External Hardware Edge Trigger Mode
 		print("Spectrometer's Current Status: {}\n".format(spectrometer.states_spectrometer))
+		print("Spectrometer's collected data: {}".format(spectrometer.data))
 	elif t_mode == "q":
 		quit()
 	else:
