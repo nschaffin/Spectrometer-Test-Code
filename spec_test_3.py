46c35f61ea5363029f6218fd61385bbadc9d6056
Ed <edwinlu8678@gmail.com>
2020-07-19 16:05:35 -0400

    quick edit

--- spec_test_3.py
+++ spec_test_3.py
@@ -28,6 +28,7 @@ def connect():
 Tests disconnection during integration
 """
 def integrate():
+	global spec1
 	try:
 		print("\nStarting integration...\n")
 		spec1.integration_time_micros(5000000)		# 5 seconds
@@ -42,19 +43,19 @@ Tests to see integration returns the same sized array
 def compare_size():
 	global spec1
 	size_array = set()
-	for _ in range(3):
+	for _ in range(4):
 		try:
 			spec1.integration_time_micros(5000)		# 5 milliseconds
 			wavelengths = spec1.wavelengths()
 			size_array.add(len(wavelengths))
 			# print(f"The wavelength size is: {len(wavelengths)}")
 		except:
-			print("Error during integration")
+			print("Error during integration. Check connection")
 	try:
 		if len(size_array) == 1:
 			print(f"Test Successful... Size of arrays are the same. The size is {size_array[0]}")
 		else:
-			print("Test Failed... Size of arrays are not the same")
+			print("Unexpected Results... Size of arrays are not the same")
 			print(f"Sizes of arrays are: {size_array}")
 	except:
 		print("The spectrometer failed to integrate.")
